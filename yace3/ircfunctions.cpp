f58c743ae185c447a1c89b6f4327a1590da3f859
giuly <giuly>
2003-12-22 16:03:45 +0000

    Changes

--- yace3/ircfunctions.cpp
+++ yace3/ircfunctions.cpp
@@ -1,3 +1,24 @@
+/*
+  Copyright (C) 2003 Giuliano Montecarlo
+
+  This file is part of YaCE 3
+
+  YaCE 3 is free software, you can redistribute it and/or
+  modify it under the terms of the Affero General Public License as
+  published by Affero, Inc., either version 1 of the License, or
+  (at your option) any later version.
+
+  YaCE 3 is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY, without even the implied warranty
+  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  Affero General Public License for more details.
+
+  You should have received a copy of the Affero General Public
+  License in the COPYING file that comes with YaCE3. If
+  not, write to Affero, Inc., 510 Third Street, Suite 225, San
+  Francisco, CA 94107 USA.
+*/
+
 #include <sstream>
 #include "stringutil.h"
 #include "ircfunctions.h"
@@ -11,6 +32,8 @@ string getChannel(const string& yaceroom)
   return yace->irc().getChannel(yaceroom);
 }
 
+
+
 void sendUserIRC(const string& user, const string& what)
 {
   ostringstream toirc;
@@ -22,6 +45,8 @@ void sendUserIRC(const string& user, const string& what)
   
   toirc << ":" << user << " PRIVMSG " << tosendto << " :" << what;
   yace->irc().send(toirc.str());  
+  
+  u->DecRef();
   return;
 }
 
@@ -34,7 +59,7 @@ void newIRCUser(const string& who)
   string ircroom = getChannel(u->getRoom());
   if(ircroom == "") return;
   
-  toirc << "NICK " << who << " 1 1 " << who << /* WHAT'S THIS??  <<  */ u->getIP() << " yace.filbboard.de 1 :Yace-User";
+  toirc << "NICK " << who << " 1 1 " << who << " " << u->getIP() << " " << yace->irc().getServerName() << " 1 :Yace-User";
   yace->irc().send(toirc.str());  
   toirc.clear();
 
@@ -44,5 +69,7 @@ void newIRCUser(const string& who)
 
   toirc << ":" << who << " JOIN " << ircroom;
   yace->irc().send(toirc.str());
+  
+  u->DecRef();
   return;
 }
