98e7f937b328340532db861d76bc6b281c566c96
chucky <chucky>
2003-12-28 14:35:25 +0000

    Fixed fucked headerthingy

--- yace3/stringutil.cpp
+++ yace3/stringutil.cpp
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2003 Giuliano Gagliardi, Thomas Yao, Giuliano Montecarlo
+  Copyright (C) 2003 Giuliano Gagliardi, Thomas Yao
 
   This file is part of YaCE 3
 
@@ -161,49 +161,39 @@ string getNoContent()
 
 
 #define NIHIL
-#define SEBISUCKS
-#define YACERULAY
-#define DONTEDIT
-#define KICKMICROSOFT
 string getHeader()
 {
   string ret;
-  // IF YOU EDIT SOME OF THIS STUFF, YOU BRAKE LAW! WE'LL FIND YOU AND KICK YOUR ASS.
-  // The YaCE-Frame don't scroll if you edit this, btw.
-  ret.append("H" DONTEDIT "TT" NIHIL "P/1" YACERULAY ".1 200" NIHIL " O" KICKMICROSOFT "K\r\n");
-  ret.append("Se" NIHIL "rv" DONTEDIT "er: Ya" NIHIL "CE" YACERULAY " 3" KICKMICROSOFT "\r\n");
-  ret.append("C" KICKMICROSOFT "ache" NIHIL "-cont" DONTEDIT "rol: no-c" NIHIL "ac" KICKMICROSOFT "he\r\n");
-  ret.append("Co" KICKMICROSOFT "nt" NIHIL "en" DONTEDIT "t-ty" NIHIL "pe: " NIHIL "tex" YACERULAY "t/h" KICKMICROSOFT "tml\r\n");
-  ret.append("" NIHIL "\r" YACERULAY "\n");
-  ret.append("\r" NIHIL "\n");
+  ret.append("HTTP/1.1 200 OK\r\n");
+  ret.append("Server: YaCE 3\r\n");
+  ret.append("Cache-control: no-cache\r\n");
+  ret.append("Content-type: text/html\r\n");
+  ret.append("\r\n");
+  ret.append("\r\n");
 
 
-  ret.append("<H" NIHIL "TML" YACERULAY ">\n");
-  ret.append("<HE" NIHIL "AD>\n");
-  ret.append("<me" NIHIL "t" DONTEDIT "a " YACERULAY "ht" NIHIL "tp-eq" DONTEDIT "uiv=\"C" NIHIL "on" KICKMICROSOFT "te" KICKMICROSOFT "nt" YACERULAY "-Type\" con" KICKMICROSOFT "t" DONTEDIT "ent" NIHIL "=\"te" KICKMICROSOFT "x" DONTEDIT "t/" YACERULAY "ht" KICKMICROSOFT "ml; c" DONTEDIT "ha" KICKMICROSOFT "rs" YACERULAY "et" KICKMICROSOFT "=UT" NIHIL "F-" KICKMICROSOFT "8\">\n");
-  ret.append("<TI" NIHIL "TLE>Ya" NIHIL "CE</T" NIHIL "I" KICKMICROSOFT "TLE>\n");
-  ret.append("<SC" NIHIL "R" DONTEDIT "IP" KICKMICROSOFT "T" NIHIL " LA" KICKMICROSOFT "NG" YACERULAY "UAG" DONTEDIT "E=\"" NIHIL "Jav" KICKMICROSOFT "a" DONTEDIT "Sc" KICKMICROSOFT "ri" YACERULAY "p" KICKMICROSOFT "t\" T" DONTEDIT "Y" KICKMICROSOFT "P" KICKMICROSOFT "E=\"tex" KICKMICROSOFT "t/" NIHIL "ja" KICKMICROSOFT "v" YACERULAY "ascri" NIHIL "p" KICKMICROSOFT "t\">\n");
+  ret.append("<HTML>\n");
+  ret.append("<HEAD>\n");
+  ret.append("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+  ret.append("<TITLE>YaCE</TITLE>\n");
+  ret.append("<SCRIPT LANGUAGE=\"JavaScript\" TYPE=\"text/javascript\">\n");
   ret.append("<!--\n");
-  ret.append("" NIHIL "s" KICKMICROSOFT "cr" DONTEDIT "ol" KICKMICROSOFT "li" YACERULAY "ng" NIHIL "=tr" NIHIL "ue;\n");
-  ret.append("fun" NIHIL "cti" SEBISUCKS "on mo" NIHIL "ve" KICKMICROSOFT "sn" DONTEDIT "ew(){\n");
-  ret.append("if (" NIHIL "sc" KICKMICROSOFT "r" YACERULAY "oll" KICKMICROSOFT "ing !" NIHIL "= fa" DONTEDIT "lse) {wind" KICKMICROSOFT "ow" YACERULAY ".scroll(1, 5" NIHIL "0" DONTEDIT "0000);}\n");
-  ret.append("wi" YACERULAY "nd" YACERULAY "ow." KICKMICROSOFT "set" NIHIL "Tim" KICKMICROSOFT "eou" KICKMICROSOFT "t(\"m" YACERULAY "oves" DONTEDIT "new" NIHIL "()\", 100);\n");
-  ret.append("}" NIHIL "\n");
-  ret.append("" NIHIL "move" DONTEDIT "sn" DONTEDIT "ew();\n");
-  ret.append("//-" NIHIL "-" KICKMICROSOFT ">" DONTEDIT "\n");
-  ret.append("</" NIHIL "SC" KICKMICROSOFT "R" DONTEDIT "IPT>\n");
-  ret.append("</H" NIHIL "E" KICKMICROSOFT "A" DONTEDIT "D>\n");
-  ret.append("<B" DONTEDIT "O" KICKMICROSOFT "DY " KICKMICROSOFT "BGC" KICKMICROSOFT "OLO" NIHIL "R=\"#f" YACERULAY "fff" KICKMICROSOFT "ff\" AL" DONTEDIT "INK=\"#000" NIHIL "000\" L" KICKMICROSOFT "IN" DONTEDIT "K=\"#000" YACERULAY "00" KICKMICROSOFT "0\" VL" DONTEDIT "INK=\"" SEBISUCKS "#00" KICKMICROSOFT "00" DONTEDIT "0\" on" YACERULAY "Foc" KICKMICROSOFT "us=\"s" SEBISUCKS "croll" DONTEDIT "ing = fa" DONTEDIT "lse\" o" SEBISUCKS "nBl" KICKMICROSOFT "ur=\"sc" KICKMICROSOFT "ro" NIHIL "lling = true\" on" DONTEDIT "Mouse" SEBISUCKS "Over=\"" SEBISUCKS "scr" DONTEDIT "olling = fa" DONTEDIT "lse\" " SEBISUCKS "onMouse" YACERULAY "Out=\"scr" YACERULAY "olling = true\">\n");
-  ret.append("<tab" SEBISUCKS "l" KICKMICROSOFT "e bo" DONTEDIT "rd" KICKMICROSOFT "er=\"1\" ce" DONTEDIT "llp" SEBISUCKS "addin" NIHIL "g=\"2\" ce" KICKMICROSOFT "l" DONTEDIT "lspac" DONTEDIT "ing" KICKMICROSOFT "=\"" SEBISUCKS "0\" " YACERULAY "s" KICKMICROSOFT "ty" DONTEDIT "le" KICKMICROSOFT "=\"pad" KICKMICROSOFT "din" DONTEDIT "g-left: 5" YACERULAY "px; bor" DONTEDIT "der-col" DONTEDIT "lapse:" NIHIL " coll" DONTEDIT "aps" KICKMICROSOFT "e\" wi" DONTEDIT "dth=\"625\">" DONTEDIT "<tr><t" DONTEDIT "d st" DONTEDIT "yle=\"width: 1" DONTEDIT "00%\"><a hr" DONTEDIT "ef=\"/SO" YACERULAY "U" SEBISUCKS "RCE/sou" DONTEDIT "rce.tar.gz\" " NIHIL "sty" YACERULAY "le=\"text" YACERULAY "-decora" YACERULAY "tion: no" DONTEDIT "ne; color: " NIHIL "#00" DONTEDIT "00" YACERULAY "00;\" tar" YACERULAY "get=\"_bla" YACERULAY "nk\">\n");
-  ret.append("<s" DONTEDIT "pan" KICKMICROSOFT " st" NIHIL "y" KICKMICROSOFT "le=\"" SEBISUCKS "fo" DONTEDIT "nt-" SEBISUCKS "family: Ta" DONTEDIT "homa, " NIHIL "Ar" KICKMICROSOFT "i" DONTEDIT "al, Time" SEBISUCKS "s" NIHIL " N" KICKMICROSOFT "ew R" DONTEDIT "om" KICKMICROSOFT "a" KICKMICROSOFT "n; fo" DONTEDIT "nt-" KICKMICROSOFT "siz" KICKMICROSOFT "e: 8pt" KICKMICROSOFT "\"><b><sp" DONTEDIT "an styl" DONTEDIT "e=\"color: #00" DONTEDIT "99FF\">" NIHIL "YaC" DONTEDIT "E 3" NIHIL "</spa" DONTEDIT "n" DONTEDIT "> " NIHIL VERSION NIHIL " - " DONTEDIT "Co" YACERULAY "pyrig" DONTEDIT "ht &" SEBISUCKS "copy; 2003" YACERULAY " Giuli" DONTEDIT "ano G" SEBISUCKS "aglia" DONTEDIT "rdi, T" YACERULAY "obias Bah" DONTEDIT "ls, Giuliano " DONTEDIT "" SEBISUCKS "Monteca" DONTEDIT "rlo, Tho" DONTEDIT "mas Yao" DONTEDIT "</b>" NIHIL "<br />Than" SEBISUCKS "ks " DONTEDIT "to: Mat" DONTEDIT "t" YACERULAY "hias Bu" DONTEDIT "tz, " NIHIL "Dani" DONTEDIT "el v.Do" YACERULAY "mbro" DONTEDIT "wski</span>");
-  ret.append("</" KICKMICROSOFT "a" SEBISUCKS "><br" KICKMICROSOFT "><s" SEBISUCKS "pan sty" KICKMICROSOFT "le=" YACERULAY "\"font" KICKMICROSOFT "-fam" KICKMICROSOFT "il" SEBISUCKS "y: Tah" SEBISUCKS "oma, Ari" NIHIL "al, Ti" KICKMICROSOFT "mes " YACERULAY "Ne" KICKMICROSOFT "w R" KICKMICROSOFT "oman, " NIHIL "Time" KICKMICROSOFT "s; fon" SEBISUCKS "t-si" KICKMICROSOFT "ze: 7p" KICKMICROSOFT "t\"></sp" KICKMICROSOFT "an>" SEBISUCKS "</t" YACERULAY "d></" YACERULAY "tr></ta" YACERULAY "ble>\n");
+  ret.append("scrolling=true;\n");
+  ret.append("function movesnew(){\n");
+  ret.append("if (scrolling != false) {window.scroll(1, 500000);}\n");
+  ret.append("window.setTimeout(\"movesnew()\", 100);\n");
+  ret.append("}\n");
+  ret.append("movesnew();\n");
+  ret.append("//-->\n");
+  ret.append("</SCRIPT>\n");
+  ret.append("</HEAD>\n");
+  ret.append("<BODY BGCOLOR=\"#ffffff\" ALINK=\"#000000\" LINK=\"#000000\" VLINK=\"#00000\" onFocus=\"scrolling = false\" onBlur=\"scrolling = true\" onMouseOver=\"scrolling = false\" onMouseOut=\"scrolling = true\">\n");
+  ret.append("<table border=\"1\" cellpadding=\"2\" cellspacing=\"0\" style=\"padding-left: 5px; border-collapse: collapse\" width=\"625\"><tr><td style=\"width: 100%\"><a href=\"/SOURCE/source.tar.gz\" style=\"text-decoration: none; color: #000000;\" target=\"_blank\">\n");
+  ret.append("<span style=\"font-family: Tahoma, Arial, Times New Roman; font-size: 8pt\"><b><span style=\"color: #0099FF\">" NIHIL "YaCE 3" NIHIL "</span> " NIHIL VERSION NIHIL " - Copyright &copy; 2003 Giuliano Gagliardi, Tobias Bahls, Giuliano Montecarlo, Thomas Yao</b><br />Thanks to: Matthias Butz, Daniel v.Dombrowski</span>");
+  ret.append("</a><br><span style=\"font-family: Tahoma, Arial, Times New Roman, Times; font-size: 7pt\"></span></td></tr></table>\n");
   return ret;
 }
 #undef NIHIL
-#undef SEBISUCKS
-#undef YACERULAY
-#undef DONTEDIT
-#undef KICKMICROSOFT
 
 void iconv(const char* to_charset, const char* from_charset, std::string& target, const std::string& source) throw (std::runtime_error)
 {
